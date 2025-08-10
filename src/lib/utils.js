3ebe50772a6d8df8b283df30aab5cb1cb271d046
exe <pintotechno@gmail.com>
2025-08-10 12:38:37 +0330

    base final

--- src/lib/utils.js
+++
@@ -1,7 +0,0 @@
-// src/lib/utils.ts
-import { clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
-// cn نهایی: ترکیب clsx + tailwind-merge
-export function cn(...inputs) {
-    return twMerge(clsx(inputs));
-}
--- src/shared/hooks/useTheme.js
+++
@@ -1,8 +0,0 @@
-import { useContext } from "react";
-import { ThemeCtx } from "@/providers/theme-context";
-export function useTheme() {
-    const ctx = useContext(ThemeCtx);
-    if (!ctx)
-        throw new Error("useTheme must be used within ThemeProvider");
-    return ctx;
-}
